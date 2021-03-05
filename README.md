7d5da66391a0ec160d07b7ab64125b21c26a9cd0
Niels <niels.dominguez@gmail.com>
2021-03-05 22:43:48 +0700

    typo

---
+++ README.md
@@ -0,0 +1,8 @@
+### Get started
+
+1 - docker-compose up
+2 - import mysql DB
+
+phpmyadmin: http://localhost:8081/ (user: MYSQL_USER psw: MYSQL_ROOT_PASSWORD)
+backend: http://localhost:5000/
+frontend: http://localhost:3000/
--- back/dist/__tests__/mockup/db.mock.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_mock_v5_1 = __importDefault(require("sequelize-mock-v5"));
-const DBConnectionMock = new sequelize_mock_v5_1.default();
-exports.default = DBConnectionMock;
-//# sourceMappingURL=db.mock.js.map
